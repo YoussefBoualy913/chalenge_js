30eb116caf137b61dcd46440cc77059a554e16d9
youssef boualy <youssefboua913@gmail.com>
2025-11-29 16:12:41 +0100

    add chalenge 33

---
+++ chalenge_33/script.js
@@ -0,0 +1,18 @@
+
+const employees = [
+    {id: 1,name: "Ahmed El Mansouri",role: "Réceptionniste",salaire: 60000,city:"paris",active:true,hireDate:"2025-3-4"},
+    {id: 2,name: "Sara Benali",role: "Technicien IT",salaire: 8000,city:"nice",active:false,hireDate:"2025-2-5"},
+    {id: 3,name: "Youssef Amrani",role: "Manager",salaire: 51000,city:"paris",active:true,hireDate:"2025-6-2"},
+    {id: 4,name: "Khadija Berrada",role: "Agent de sécurité",salaire: 4000,city:"raba",active:false,hireDate:"2025-3-6"},
+    {id: 5,name: "Omar Essafi",role: "Nettoyage",salaire: 3000,city:"paris",active:true,hireDate:"2025-6-4"}
+];
+
+const informationgenerale=document.getElementById('informationgenerale');
+const listname=document.createElement('ul');
+
+const listemployee=employees.reduce((acc,elm)=>{
+    
+    return acc+`<li>${elm.name}-${elm.role}</li>`;
+},'');
+listname.innerHTML=listemployee;
+informationgenerale.append(listname);
